58a4e77eb4b1f42c5fe7548f98b1d15b16984400
Tim Lolkema <mail@timlolkema.nl>
2025-12-10 18:27:42 +0100

    Ticket #6: Combine identical line items in basket with quantity display

--- shop.js
+++ shop.js
@@ -37,11 +37,18 @@ function renderBasket() {
     if (cartButtonsRow) cartButtonsRow.style.display = "none";
     return;
   }
-  basket.forEach((product) => {
-    const item = PRODUCTS[product];
+  // Count occurrences of each product
+  const productCounts = basket.reduce((counts, product) => {
+    counts[product] = (counts[product] || 0) + 1;
+    return counts;
+  }, {});
+  // Render grouped items with quantities
+  Object.keys(productCounts).forEach((productId) => {
+    const item = PRODUCTS[productId];
     if (item) {
+      const quantity = productCounts[productId];
       const li = document.createElement("li");
-      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${item.name}</span>`;
+      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${quantity}x ${item.name}</span>`;
       basketList.appendChild(li);
     }
   });
